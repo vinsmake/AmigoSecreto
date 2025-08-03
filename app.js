e1fe7cb8c51b26fc687d88b415a386c067e78cb9
vinsmake <ps.enrique.e@gmail.com>
2025-08-02 20:01:30 -0600

    Amigo secreto woking

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+        return;
+    }
+    const indice = Math.floor(Math.random() * amigos.length);
+    const nombreSorteado = amigos[indice];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>${nombreSorteado}</li>`;
+}
+let amigos = []
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+    if (!nombre) {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    amigos.push(nombre);
+    input.value = '';
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
